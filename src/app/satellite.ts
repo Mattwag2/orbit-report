64f0d20b5d8662414245d8f3a4f3e8f5bbbe0a82
Mattwag2 <72990180+Mattwag2@users.noreply.github.com>
2021-01-05 13:06:16 -0600

    Update satellite.ts

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -15,7 +15,7 @@ export class Satellite {
     } 
 
     shouldShowWarning() {
-        if (this.type = "Space Debris"){
+        if (this.type == "Space Debris"){
             return true
         }
     }
